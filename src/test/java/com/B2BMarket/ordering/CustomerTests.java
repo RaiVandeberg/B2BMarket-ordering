0a0c0e8e7a2cce0711df00ca08e19abcfda9cc41
Rai Vandenberg <rvdesigner20@gmail.com>
2025-12-18 22:53:14 -0300

    feat add constructor and setter in class customer

---
+++ src/test/java/com/B2BMarket/ordering/CustomerTests.java
@@ -0,0 +1,29 @@
+package com.B2BMarket.ordering;
+
+import com.B2BMarket.ordering.domain.entity.Customer;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class CustomerTests {
+
+    @Test
+    public void testingCustomers(){
+        Customer customer = new Customer(
+                UUID.randomUUID(),
+                "Rai Braz",
+                LocalDate.of(1991, 7, 5),
+                "raibs@email.com",
+                "487-415-2504",
+                "255-08-0578",
+                true,
+                OffsetDateTime.now()
+        );
+
+        customer.addLoyaltyPoints(10);
+
+    }
+
+}
